a8986d25703ace66648beb43dd52cf79e9d99aaf
eddie-99 <edsong.arreola99@gmail.com>
2022-12-07 21:24:28 -0600

    data para mostrar la informacion de las peliculas

---
+++ script.js
@@ -0,0 +1,47 @@
+let data = [
+    {
+        id: "tt5727208",
+        titulo: "Uncut Gems",
+        director: " Benny Safdie, Josh Safdie",
+        anioDeEstreno: 2019,
+        paisDeOrigen: ["USA"],
+        generos: ["Crime", "Drama", "Thriller"],
+        calificacion: 7.4
+    },
+    {
+        id: "tt1379182",
+        titulo: "Dogtooth",
+        director: "Yorgos Lanthimos",
+        anioDeEstreno: 2009,
+        paisDeOrigen: ["Greece"],
+        generos: ["Drama", "Thriller"],
+        calificacion: 7.3
+    },
+    {
+        id: "tt5715874",
+        titulo: "The Killing of a Sacred Deer",
+        director: "Yorgos Lanthimos",
+        anioDeEstreno: 2017,
+        paisDeOrigen: ["USA"],
+        generos: ["Drama", "Horror", "Mystery"],
+        calificacion: 7.0
+    },
+    {
+        id: "ttt0903747",
+        titulo: "Breaking Bad",
+        director: "Vince Gilligan",
+        anioDeEstreno: 2008,
+        paisDeOrigen: ["USA"],
+        generos: ["Crime", "Drama", "Thriller"],
+        calificacion: 9.5
+    }, 
+    {
+        id: "tt2861424",
+        titulo: "Rick and Morty",
+        director: "Dan HarmonJustin, Roiland",
+        anioDeEstreno: 2013,
+        paisDeOrigen: ["USA"],
+        generos: ["Animation", "Adventure", "Comedy"],
+        calificacion: 9.1
+    }
+];
